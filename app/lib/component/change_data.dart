033857c75fc2998b2cef4a98002bb7ddbb609dda
Kamiyama <kkimh256@icloud.com>
2025-03-04 02:13:53 +0900

    feat: 検索結果テーブルの表示実装

--- app/lib/component/change_data.dart
+++ app/lib/component/change_data.dart
@@ -2,15 +2,21 @@ import 'dart:convert';
 
 class ChangeData {
   Future<Map<String, Map<String, String>>> Repo(String data) async {
-    Map<String, Map<String, String>> repoMap = {};
-    final changeData = jsonDecode(data);
-    for (var item in changeData['items']) {
-      repoMap[item['name']] = {
-        'repo_name': item['name'],
-        'owner_icon': item['owner']['avatar_url'],
-        'owner_name': item['owner']['login'],
-      };
+    try {
+      Map<String, Map<String, String>> repoMap = {};
+      final changeData = jsonDecode(data);
+      for (var item in changeData['items']) {
+        repoMap[item['name']] = {
+          'repo_name': item['name'],
+          'owner_icon': item['owner']['avatar_url'],
+          'owner_name': item['owner']['login'],
+        };
+      }
+      return repoMap;
+    } catch (e) {
+      print(e);
+      print('Failed to change data');
+      return {};
     }
-    return repoMap;
   }
 }
--- app/lib/home.dart
+++ app/lib/home.dart
@@ -1,13 +1,17 @@
 import 'package:app/api/api_request.dart';
+import 'package:app/component/change_data.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends ConsumerWidget {
-  final textProvider = StateProvider<String>((ref) => "No Data");
+  final visibilityProvider = StateProvider<bool>((ref) => false);
+  final repositoriesProvider =
+      StateProvider<Map<String, Map<String, String>>>((ref) => {});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final TextEditingController textEditingController = TextEditingController();
-    final text = ref.watch(textProvider);
+    final visibility = ref.watch(visibilityProvider);
+    final repoData = ref.watch(repositoriesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
@@ -26,16 +30,69 @@ class HomePage extends ConsumerWidget {
                   controller: textEditingController,
                 ),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  final data = await ApiRequest().fetchApiRepo("MukiMuki", ref);
-                  ref.read(textProvider.notifier).state = data;
-                  print("Home: $text");
-                  print("Home: $data");
-                },
-                child: const Text('Fetch Data'),
+              Padding(
+                padding: EdgeInsets.all(5),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (textEditingController.text.isEmpty) {
+                      return;
+                    }
+                    final data = await ApiRequest()
+                        .fetchApiRepo(textEditingController.text, ref);
+                    final repositories = await ChangeData().Repo(data);
+                    ref.read(repositoriesProvider.notifier).state =
+                        repositories;
+                    ref.read(visibilityProvider.notifier).state = true;
+                  },
+                  child: const Text('検索'),
+                ),
               ),
-              Text(text),
+              Visibility(
+                visible: visibility,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Table(
+                        border: TableBorder.all(),
+                        children: [
+                          TableRow(
+                            children: [
+                              Center(
+                                child: Text("Owner_icon", style: TextStyle()),
+                              ),
+                              Center(
+                                child: Text("Owner_name"),
+                              ),
+                              Center(
+                                child: Text("Repo_name"),
+                              ),
+                            ],
+                          ),
+                          for (var item in repoData.entries)
+                            TableRow(
+                              children: [
+                                Image.network(
+                                  item.value['owner_icon']!,
+                                  width: 50,
+                                  height: 50,
+                                ),
+                                Center(
+                                  child: Text(item.value['owner_name']!),
+                                ),
+                                Center(
+                                  child: Text(
+                                    item.value['repo_name']!,
+                                  ),
+                                ),
+                              ],
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
